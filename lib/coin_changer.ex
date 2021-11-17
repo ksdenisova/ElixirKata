6ae7abcf0db97bcabebdfd704754121414b4125a
Kseniia Denisova <kdenisova@8thlight.com>
2021-11-17 11:43:00 -0800

    Implement different make_change methods

--- lib/coin_changer.ex
+++ lib/coin_changer.ex
@@ -1,5 +1,29 @@
 defmodule CoinChanger do
-    def make_change(coins) do
-        %{:pennies => 0, :nickels => 0, :dimes => 0, :quarters => 0}
+    def make_change(change, 0) do
+        change
+    end
+
+    def make_change(change, coins) when coins >= 25 do
+        change = %{change | :quarters => change[:quarters] + 1}
+        quarter = 25
+        make_change(change, coins - quarter)
+    end
+
+    def make_change(change, coins) when coins >= 10 do
+        change = %{change | :dimes => change[:dimes] + 1}
+        dime = 10
+        make_change(change, coins - dime)
+    end
+
+    def make_change(change, coins) when coins >= 5 do
+        change = %{change | :nickels => change[:nickels] + 1}
+        nickel = 5
+        make_change(change, coins - nickel)
+    end
+
+    def make_change(change, coins) when coins >= 1 do
+        change = %{change | :pennies => change[:pennies] + 1}
+        penny = 1
+        make_change(change, coins - penny)
     end
 end
--- test/coin_changer_test.exs
+++ test/coin_changer_test.exs
@@ -3,41 +3,114 @@ defmodule CoinChangerTest do
 
   alias CoinChanger
 
-  doctest CoinChanger
-
   test "module exists" do
     assert is_list(CoinChanger.module_info())
   end
 
   test "returns a change with pennies" do
-    change = CoinChanger.make_change(0)
+    change = %{:pennies => 0, :nickels => 0, :dimes => 0, :quarters => 0}
+             |> CoinChanger.make_change(0)
 
     assert Map.has_key?(change, :pennies)
   end
 
   test "returns a change with nickels" do
-    change = CoinChanger.make_change(0)
+    change = %{:pennies => 0, :nickels => 0, :dimes => 0, :quarters => 0}
+    change = CoinChanger.make_change(change, 0)
 
     assert Map.has_key?(change, :nickels)
   end
 
   test "returns a change with dimes" do
-    change = CoinChanger.make_change(0)
+    change = %{:pennies => 0, :nickels => 0, :dimes => 0, :quarters => 0}
+    change = CoinChanger.make_change(change, 0)
 
     assert Map.has_key?(change, :dimes)
   end
 
   test "returns a change with quarters" do
-    change = CoinChanger.make_change(0)
+    change = %{:pennies => 0, :nickels => 0, :dimes => 0, :quarters => 0}
+    change = CoinChanger.make_change(change, 0)
 
     assert Map.has_key?(change, :quarters)
   end
 
   test "returns all zero if 0 was given" do
     coins = 0
-    actual = CoinChanger.make_change(coins) |> Map.values
+    actual = %{:pennies => 0, :nickels => 0, :dimes => 0, :quarters => 0}
+             |> CoinChanger.make_change(coins)
     
-    expected = [0, 0, 0, 0]
+    expected = %{:pennies => 0, :nickels => 0, :dimes => 0, :quarters => 0}
+
+    assert actual == expected
+  end
+
+  test "returns an 1 penny for 1 coin given" do
+    coins = 1
+    actual = %{:pennies => 0, :nickels => 0, :dimes => 0, :quarters => 0}
+             |> CoinChanger.make_change(coins)
+
+    expected = %{:pennies => 1, :nickels => 0, :dimes => 0, :quarters => 0}
+
+    assert actual == expected
+  end
+
+  test "returns an 2 pennies for 2 coins given" do
+    coins = 2
+    actual =  %{:pennies => 0, :nickels => 0, :dimes => 0, :quarters => 0}
+              |> CoinChanger.make_change(coins)
+
+    expected = %{:pennies => 2, :nickels => 0, :dimes => 0, :quarters => 0}
+
+    assert actual == expected
+  end
+
+  test "returns an 3 quarters for 75 coins given" do
+    coins = 75
+    actual =  %{:pennies => 0, :nickels => 0, :dimes => 0, :quarters => 0}
+              |> CoinChanger.make_change(coins)
+
+    expected = %{:pennies => 0, :nickels => 0, :dimes => 0, :quarters => 3}
+
+    assert actual == expected
+  end
+
+  test "returns an 3 quarters and 3 pennies for 78 coins given" do
+    coins = 78
+    actual =  %{:pennies => 0, :nickels => 0, :dimes => 0, :quarters => 0}
+              |> CoinChanger.make_change(coins)
+
+    expected = %{:pennies => 3, :nickels => 0, :dimes => 0, :quarters => 3}
+
+    assert actual == expected
+  end
+
+  test "returns an 4 quarters, 1 dime and 3 pennies for 113 coins given" do
+    coins = 113
+    actual =  %{:pennies => 0, :nickels => 0, :dimes => 0, :quarters => 0}
+              |> CoinChanger.make_change(coins)
+
+    expected = %{:pennies => 3, :nickels => 0, :dimes => 1, :quarters => 4}
+
+    assert actual == expected
+  end
+
+  test "returns an 1 quarter, 1 dime, 1 nickel and 1 penny for 41 coins given" do
+    coins = 41
+    actual =  %{:pennies => 0, :nickels => 0, :dimes => 0, :quarters => 0}
+              |> CoinChanger.make_change(coins)
+
+    expected = %{:pennies => 1, :nickels => 1, :dimes => 1, :quarters => 1}
+
+    assert actual == expected
+  end
+
+  test "returns an 1 nickel for 5 coins given" do
+    coins = 5
+    actual =  %{:pennies => 0, :nickels => 0, :dimes => 0, :quarters => 0}
+              |> CoinChanger.make_change(coins)
+
+    expected = %{:pennies => 0, :nickels => 1, :dimes => 0, :quarters => 0}
 
     assert actual == expected
   end
